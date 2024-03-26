057b2ba62cde4c0a74beadeba23b11995341ca98
emilianocampos <emicampos1268@gmail.com>
2024-03-26 20:45:32 -0300

    sad

--- js/main.js
+++ js/main.js
@@ -2,6 +2,21 @@ ScrollReveal().reveal('.imgpca');
 AOS.init();
 
 var typed = new Typed('#typed', {
-    stringsElement: '#typed-strings',
-    typeSpeed: 30
+  stringsElement: '#typed-strings',
+  typeSpeed: 30
+});
+const demoLink = document.getElementById('demogra');
+
+demoLink.addEventListener('click', () =>{
+setTimeout(() => {
+  Swal.fire({
+    title: "Que es solicitar una demo?",
+    text: "Al solicitar una demo, me estas pidiendo que cree el diseño de tu sitio web, por lo tanto en el area de CONSULTA deberias de describir como seria tu sitio web ideal",
+  
+    icon: "info"
   });
+  
+}, 2300);
+
+
+})
